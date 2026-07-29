5bed9c4d9c207e13b98725f6994c8f7971cff8f3
Nishant Joshi <70685741+nishant-10@users.noreply.github.com>
2026-07-29 22:39:58 +0530

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let left = 0;
+        let right = nums.length - 1;
+        let mid = Math.floor((left + right) / 2);
+        while (left <= right) {
+            if (target > nums[mid]) {
+                left = mid + 1;
+                mid = Math.floor((left + right) / 2);
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+                mid = Math.floor((left + right) / 2);
+            } else if (target === nums[mid]) {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
